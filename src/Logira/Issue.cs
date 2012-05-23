9959aeb64e2bcf79a6d9a6a8b97d9b074fd6d2d3
sukchr <sukchr@gmail.com>
2012-05-23 20:13:36 +0200

    New properties on Issue.

--- src/Logira/Issue.cs
+++ src/Logira/Issue.cs
@@ -1,3 +1,5 @@
+using Brevity;
+
 namespace Logira
 {
     /// <summary>
@@ -9,7 +11,17 @@ namespace Logira
         /// Gets the key of the issue. E.g. "TST-123".
         /// </summary>
         public string Key { get; private set; }
-
+        /// <summary>
+        /// Gets the project key of the issue. E.g. "TST".
+        /// </summary>
+        public string ProjectKey { get; private set; }
+        /// <summary>
+        /// Gets the issue counter of the issue. E.g. "123". 
+        /// </summary>
+        public int IssueCounter { get; private set; }
+        /// <summary>
+        /// Gets the URL of the issue. Uses <see cref="Jira.BrowseIssueUrl"/> to build the full URL of the issue. 
+        /// </summary>
         public string Url
         {
             get { return Jira.BrowseIssueUrl + Key; }
@@ -18,6 +30,9 @@ namespace Logira
         internal Issue(string key)
         {
             Key = key;
+            var parts = key.Split('-');
+            ProjectKey = parts[0];
+            IssueCounter = parts[1].ToInt();
         }
     }
 }
