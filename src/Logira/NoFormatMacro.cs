2875406da5c23fbc04a3d7dcea7a0ed530eb196b
sukchr <sukchr@gmail.com>
2012-06-23 14:11:10 +0200

    NoFormatMacro can have title.

--- src/Logira/NoFormatMacro.cs
+++ src/Logira/NoFormatMacro.cs
@@ -8,14 +8,20 @@ namespace Logira
     public class NoFormatMacro : IMacro
     {
         /// <summary>
+        /// The macro's title: {noformat:title=The title}
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
         /// The contents of the noformat macro.
         /// </summary>
         public string Contents { get; set; }
 
         public string Render()
         {
-            return @"{noformat}$contents${noformat}"
-                .Set("contents", Contents);
+            return @"{noformat:title=$title$}$contents${noformat}"
+                .Set("contents", Contents)
+                .Set("title", Title);
         }
     }
 }
